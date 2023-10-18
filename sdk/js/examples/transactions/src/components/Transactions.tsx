809b37103fe7de12ea7dbdce32c24d1f84189fd4
Wouter Nederhof <wouter.nederhof@gmail.com>
2023-10-18 22:27:18 +0200

    WIP JS SDK example POC for transactions
    GitOrigin-RevId: c878b09270c6f2fd70c2d2a64d223914d6caff69

---
+++ sdk/js/examples/transactions/src/components/Transactions.tsx
@@ -0,0 +1,3 @@
+export const Transactions = () => (
+    <table></table>
+)
---
+++ sdk/js/examples/transactions/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { Transactions } from "@/components/Transactions";
+
+export default function Home() {
+  return <>
+    <Transactions />
+  </>
+}
