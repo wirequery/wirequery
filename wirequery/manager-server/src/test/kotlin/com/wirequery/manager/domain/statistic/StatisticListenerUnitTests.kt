97433b68084c9a580cc2cea6c9a51b25d0572949
Wouter Nederhof <wouter.nederhof@gmail.com>
2023-11-30 16:12:13 +0100

    Add missing tests. Fix statistics GraphQL schema.
    GitOrigin-RevId: 0d1c0453d315cefd5b590390824ec1a0ce8aa27e

--- wirequery/manager-server/src/test/kotlin/com/wirequery/manager/domain/statistic/StatisticListenerUnitTests.kt
+++ wirequery/manager-server/src/test/kotlin/com/wirequery/manager/domain/statistic/StatisticListenerUnitTests.kt
@@ -7,19 +7,57 @@
 
 package com.wirequery.manager.domain.statistic
 
+import com.wirequery.manager.domain.querylog.QueryLogEvent
+import com.wirequery.manager.domain.querylog.QueryLogFixtures.QUERY_LOG_FIXTURE_WITH_ID_1
+import com.wirequery.manager.domain.user.UserEvent
+import com.wirequery.manager.domain.user.UserFixtures.USER_FIXTURE_WITH_ID_1
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito.verify
 import org.mockito.junit.jupiter.MockitoExtension
 
 @ExtendWith(MockitoExtension::class)
 internal class StatisticListenerUnitTests {
-
     @Mock
     private lateinit var statisticService: StatisticService
 
     @InjectMocks
     private lateinit var statisticListener: StatisticListener
 
-    // TODO test
+    @Test
+    fun `When QueryLogsCreatedEvent is triggered, the related statistic is incremented`() {
+        statisticListener.onEvent(QueryLogEvent.QueryLogsCreatedEvent(this, listOf(QUERY_LOG_FIXTURE_WITH_ID_1)))
+
+        verify(statisticService)
+            .increment(
+                StatisticService.IncrementStatisticInput(
+                    type = Statistic.TypeEnum.QUERY_LOG,
+                    metadata =
+                        mapOf(
+                            "storedQueryId" to "" + QUERY_LOG_FIXTURE_WITH_ID_1.storedQueryId,
+                            "appName" to QUERY_LOG_FIXTURE_WITH_ID_1.appName,
+                        ),
+                    amount = 1,
+                ),
+            )
+    }
+
+    @Test
+    fun `When UsersLoggedInEvent is triggered, the related statistic is incremented`() {
+        statisticListener.onEvent(UserEvent.UsersLoggedInEvent(this, listOf(USER_FIXTURE_WITH_ID_1.username)))
+
+        verify(statisticService)
+            .increment(
+                StatisticService.IncrementStatisticInput(
+                    type = Statistic.TypeEnum.LOGIN,
+                    metadata =
+                        mapOf(
+                            "username" to "" + USER_FIXTURE_WITH_ID_1.username,
+                        ),
+                    amount = 1,
+                ),
+            )
+    }
 }
