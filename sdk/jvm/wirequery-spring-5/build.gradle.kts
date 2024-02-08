89df55117c6088b84ac11c3fe7ef934bac9fa0e2
Wouter Nederhof <wouter.nederhof@gmail.com>
2024-02-08 20:32:56 +0100

    Update creds in build gradle files
    GitOrigin-RevId: 21435e9973768a98a12a94cee52e6f8b610cd369

--- sdk/jvm/wirequery-spring-5/build.gradle.kts
+++ sdk/jvm/wirequery-spring-5/build.gradle.kts
@@ -117,8 +117,8 @@ publishing {
             name = "OSSRH"
             url = URI("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
             credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
+                username = System.getenv("OSSRH_USERNAME")
+                password = System.getenv("OSSRH_PASSWORD")
             }
         }
     }
